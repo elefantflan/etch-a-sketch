69d6a19742441d2c821249180e1703878247de3b
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-07-10 22:46:22 -0700

    retry nesting row>column

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 
+<<<<<<< HEAD
 function defaultGrid(rows, columns)
     {   
             for(i=1;i<=16;i++)
@@ -20,3 +21,17 @@ defaultGrid();
 
 
 
+=======
+function defaultRow(rows)
+    {
+        for(i=1;i<=16;i++)
+            {
+                rows = document.createElement('div')
+                rows.id = "row " +i
+                console.log(i)
+                console.log(rows);
+                container.appendChild(rows);
+            }
+            
+    }
+>>>>>>> cfa71f7 (Succes: Row divs made inside container)
