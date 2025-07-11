00216ca0763e8f352f324fbc8e399e74d445c3f9
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-07-11 11:56:39 -0700

    columns successfully nested inside rows!

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
 const container = document.querySelector(".container");
 
-<<<<<<< HEAD
-function defaultGrid(rows, columns)
-    {   
-            for(i=1;i<=16;i++)
-                {
-                    rows = document.createElement('div')
-                    rows.id = "row" +i;
-                    for(i=1;i<=16;i++)
-                        {
-                            columns = document.createElement('div')
-                            columns.id = "columns " +i;
-                            rows.appendChild(columns);
-                        }
-                }
-        
-        container.appendChild(rows);
-    }
-defaultGrid();
 
 
-
-=======
 function defaultRow(rows)
-    {
+    {  
         for(i=1;i<=16;i++)
             {
-                rows = document.createElement('div')
-                rows.id = "row " +i
-                console.log(i)
-                console.log(rows);
+                rows = document.createElement('div');
+                rows.id = "rows " +i;
+                    for(j=1;j<=16;j++)
+                    {
+                        columns = document.createElement('div')
+                        columns.id = "columns " +j
+                        console.log(columns);
+                        rows.appendChild(columns);
+                    }
                 container.appendChild(rows);
             }
-            
+
     }
->>>>>>> cfa71f7 (Succes: Row divs made inside container)
+defaultRow();
