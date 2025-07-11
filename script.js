21705108f4285159cee3ff325d32cdbc5d5d69d0
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-07-10 23:02:03 -0700

    columns go into one row but it doesn't loop until 16 rows are created

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const container = document.querySelector(".container");
 
-function defaultRow(rows)
+function defaultGrid(rows, columns)
     {
-        for(i=1;i<=16;i++)
-            {
-                rows = document.createElement('div')
-                rows.id = "row " +i
-                console.log(i)
-                console.log(rows);
-                container.appendChild(rows);
-            }
-            
+            for(i=1;i<=16;i++)
+                {
+                    rows = document.createElement('div')
+                    rows.id = "row " +i;
+                    for(i=1;i<=16;i++)
+                        {
+                            columns = document.createElement('div')
+                            columns.id = "columns " +i;
+                            rows.appendChild(columns);
+                        }
+                }
+        
+        container.appendChild(rows);
     }
+
+defaultGrid();
