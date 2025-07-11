3d59893770410bb521fb7900b70ffc01c3d43bd5
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-07-11 13:09:33 -0700

    new branch for advanced grids

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ const container = document.querySelector(".container");
 
 
 
-function defaultRow(rows, columns)
+function defaultGrid()
     {  
         for(i=1;i<=16;i++)
             {
-                rows = document.createElement('div');
+                const rows = document.createElement('div');
                 rows.classList.add("row", "row-" +i);
                     for(j=1;j<=16;j++)
                     {
-                        columns = document.createElement('div');
+                        const columns = document.createElement('div');
                         columns.classList.add("column", "column-" +j);
                         rows.appendChild(columns);
                     }
                 container.appendChild(rows);
             }
     }
-defaultRow();
-
+defaultGrid();
 const columns = document.querySelectorAll('.column');
 columns.forEach(column =>
         {
@@ -39,5 +38,37 @@ reset.addEventListener('click', ()=>
                         column.style.backgroundColor='black';
                     }
                 )
+        }
+)
+
+function canvasGrid()
+        {
+
+        }
+
+const canvas = document.querySelector('#canvas');
+canvas.addEventListener('click', ()=>
+        {
+            const userInput = Number(prompt('Enter 16-100'))
+            
+            const min = 16;
+            const max = 100;
+
+            console.log(typeof userInput);
+
+            if (userInput >=16 && userInput <=100)
+                {
+                    columns.forEach(column =>
+                            {
+                                column.style.backgroundColor='black';
+                            }
+                        )
+                    defaultGrid(userInput, userInput);
+                }
+            else
+                {
+                    alert('Try Again')
+                }
+                
         }
 )
