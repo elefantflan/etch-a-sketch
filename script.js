e00f97aea2e2c6ed58cf3ebb57d064b0b3382b29
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-07-10 23:16:34 -0700

    wtf

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector(".container");
 
 function defaultGrid(rows, columns)
-    {
+    {   
             for(i=1;i<=16;i++)
                 {
                     rows = document.createElement('div')
-                    rows.id = "row " +i;
+                    rows.id = "row" +i;
                     for(i=1;i<=16;i++)
                         {
                             columns = document.createElement('div')
@@ -16,5 +16,7 @@ function defaultGrid(rows, columns)
         
         container.appendChild(rows);
     }
-
 defaultGrid();
+
+
+
